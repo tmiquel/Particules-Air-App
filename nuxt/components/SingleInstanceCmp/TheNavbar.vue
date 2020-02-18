57060da67b735d8b580dafa0020deb7554a5a627
Thibaut Miquel <thibaut.miquel@gmail.com>
2020-02-18 17:52:00 +0100

    feat - navbar with bootstrap-vue

--- nuxt/components/SingleInstanceCmp/TheNavbar.vue
+++ nuxt/components/SingleInstanceCmp/TheNavbar.vue
@@ -1,64 +1,40 @@
 <template>
-  <nav
-    class="navbar fixed-top navbar-toggleable-sm navbar-expand-md"
-  >
-    <button
-      class="navbar-toggler navbar-toggler-left collapsed"
-      type="button"
-      data-toggle="collapse"
-      data-target="#collapsingNavbar"
+  <b-navbar toggleable="md" fixed="top" type="dark">
+    <b-navbar-toggle 
+      target="collapsing-navbar"
+      class="border-0 "
     >
-      <span> </span>
-      <span> </span>
-      <span> </span>
-    </button>
-    <a class="navbar-brand mr-auto ml-auto" href="#">
-      PARTICULES <i class="hidden-sm-down"></i>
-    </a>
-    <div class="collapse navbar-collapse" id="collapsingNavbar">
-      <ul class="nav navbar-nav ml-auto">
-        <li class="nav-item d-block d-md-none">
-          <a class="nav-link" href="#">Accueil</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">A propos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
-        <li class="nav-item d-none d-md-block">
-          <a class="nav-link" href="#">Mode Sombre</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+
+    </b-navbar-toggle >
+    <b-navbar-brand class="mr-auto ml-auto" href="#">PARTICULES</b-navbar-brand>
+    <b-collapse id="collapsing-navbar" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item class="d-block d-md-none" href="#">Accueil</b-nav-item>
+        <b-nav-item href="#">A propos</b-nav-item>
+        <b-nav-item href="#">Contact</b-nav-item>
+        <b-nav-item href="#">Mode Sombre</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <style lang="scss">
+.navbar-toggler > .navbar-toggler-icon {
+  fill: white; 
+  color: white;
+}
 
-
-// .nav-link:not(:hover):not(:active) {
-//   color: #ffffff !important; 
-// }
-
-
-
-
-.nav-link{
-  color: #fff !important;//$gray-100 !important; 
+.nav-link {
+  color: #fff !important; //$gray-100 !important;
 }
 
 .nav-link:hover {
-  // color: blue!important;
   color: gray("300") !important;
-  // background-color: var(--left-gradient-color);
 }
 
-.nav-link:active  {
-  // color: #fff !important; 
+.nav-link:active {
   background-color: var(--left-gradient-color);
 }
-
 
 nav.navbar {
   background: linear-gradient(
@@ -78,64 +54,7 @@ nav.navbar {
 }
 
 body {
-    padding-top: 70px;
-    font-family: 'Barlow', sans-serif;
-}
-
-.navbar-toggler:focus,
-.navbar-toggler:active {
-    outline: 0;
-}
-
-.navbar-toggler span {
-   display: block;
-   background-color: #ffffff;
-   height: 3px;
-   width: 25px;
-   margin-top: 4px;
-   margin-bottom: 4px;
-   -webkit-transform: rotate(0deg);
-   -moz-transform: rotate(0deg);
-   -o-transform: rotate(0deg);
-   transform: rotate(0deg);
-   position: relative;
-   left: 0;
-   opacity: 1;
-}
-
-.navbar-toggler span:nth-child(1),
-.navbar-toggler span:nth-child(3) {
-   -webkit-transition: transform .35s ease-in-out;
-   -moz-transition: transform .35s ease-in-out;
-   -o-transition: transform .35s ease-in-out;
-   transition: transform .35s ease-in-out;
-}
-
-.navbar-toggler:not(.collapsed) span:nth-child(1) {
-    position: absolute;
-    left: 12px;
-    top: 10px;
-    -webkit-transform: rotate(135deg);
-    -moz-transform: rotate(135deg);
-    -o-transform: rotate(135deg);
-    transform: rotate(135deg);
-    opacity: 0.9;
-}
-
-.navbar-toggler:not(.collapsed) span:nth-child(2) {
-    height: 12px;
-    visibility: hidden;
-    background-color: transparent;
-}
-
-.navbar-toggler:not(.collapsed) span:nth-child(3) {
-    position: absolute;
-    left: 12px;
-    top: 10px;
-    -webkit-transform: rotate(-135deg);
-    -moz-transform: rotate(-135deg);
-    -o-transform: rotate(-135deg);
-    transform: rotate(-135deg);
-    opacity: 0.9;
+  padding-top: 70px;
+  font-family: "Barlow", sans-serif;
 }
 </style>
