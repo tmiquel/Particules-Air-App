cd14f6af8d3d76bfa6fd695d21b7f758ec91b88f
Kevin Berthier <kevinberthier@orange.fr>
2020-02-20 14:49:55 +0100

    refacto

--- nuxt/components/SingleInstanceCmp/TheFooter.vue
+++ nuxt/components/SingleInstanceCmp/TheFooter.vue
@@ -11,47 +11,48 @@
         squared
         variant="primary"
         class="footer-item px-0 flex-grow-1 justify-content-center"
-        ><span :style="footerTextStyle"
-          >{{ responsiveGraphText }} <b-icon-pie-chart class="ml-1" />
+      >
+        <span :style="footerTextStyle">
+          {{ responsiveGraphText }}
+          <b-icon-pie-chart class="ml-1" />
         </span>
       </b-button>
       <b-button
         squared
         variant="primary"
         class="footer-item px-0 flex-grow-1 justify-content-center"
-        ><span :style="footerTextStyle"
-          >{{ responsiveStakeholdersText }}<b-icon-people class="ml-1"
-        /></span>
+      >
+        <span :style="footerTextStyle">
+          {{ responsiveStakeholdersText }}
+          <b-icon-people class="ml-1" />
+        </span>
       </b-button>
       <b-button
         squared
         variant="primary"
         class="footer-item px-0 flex-grow-1 justify-content-center"
-        ><span :style="footerTextStyle"
-          >{{ responsiveDefinitionsText }}
-          <b-icon-list-check class="ml-1" scale="1.4" shift-v="-1"
-        /></span>
+      >
+        <span :style="footerTextStyle">
+          {{ responsiveDefinitionsText }}
+          <b-icon-list-check class="ml-1" scale="1.4" shift-v="-1" />
+        </span>
       </b-button>
       <b-button
         squared
         variant="primary"
         class="footer-item px-0 flex-grow-1 justify-content-center"
-        ><span :style="footerTextStyle"
-          >{{ responsiveSourcesText }}<b-icon-documents class="ml-1"
-        /></span>
+      >
+        <span :style="footerTextStyle">
+          {{ responsiveSourcesText }}
+          <b-icon-documents class="ml-1" />
+        </span>
       </b-button>
     </b-nav>
   </div>
-</template>  
+</template>
 
 <script>
-import {
-  BIcon,
-  BIconPieChart,
-  BIconPeople,
-  BIconListCheck,
-  BIconDocuments
-} from "bootstrap-vue";
+import { BIcon, BIconPieChart, BIconPeople, BIconListCheck, BIconDocuments } from 'bootstrap-vue'
 
 export default {
   components: {
@@ -63,39 +64,31 @@ export default {
   },
   computed: {
     responsiveGraphText() {
-      return this.$mq === "mobile" ? "GRAPHES" : "LES GRAPHES";
+      return this.$mq === 'mobile' ? 'GRAPHES' : 'LES GRAPHES'
     },
     responsiveStakeholdersText() {
-      return this.$mq === "mobile" ? "ACTEURS" : "LES ACTEURS";
+      return this.$mq === 'mobile' ? 'ACTEURS' : 'LES ACTEURS'
     },
     responsiveDefinitionsText() {
-      return this.$mq === "mobile" ? "LEXIQUE" : "LE LEXIQUE";
+      return this.$mq === 'mobile' ? 'LEXIQUE' : 'LE LEXIQUE'
     },
     responsiveSourcesText() {
-      return this.$mq === "mobile" ? "SOURCES" : "LES SOURCES";
+      return this.$mq === 'mobile' ? 'SOURCES' : 'LES SOURCES'
     },
     footerTextStyle() {
-      if (this.$mq === "mobile") {
-        return {
-          fontSize: "0.7rem"
-        };
-      } else {
-        return {
-          fontSize: "1rem"
-        };
+      return {
+        fontSize: this.$mq === 'mobile' ? '0.7rem' : '1rem'
       }
     }
   }
-};
+}
 </script>
 
 
 <style scoped>
 .footer-item span {
-  font-family: "Source Sans Pro", "Barlow", -apple-system, BlinkMacSystemFont,
-      "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif,
-      "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol",
-      "Noto Color Emoji";
+  font-family: 'Source Sans Pro', 'Barlow', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue',
+    Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
 }
 </style>
 
