3c38d81551807d2c1fe029e4070b2be7721ab93f
Kevin Berthier <kevinberthier@orange.fr>
2020-02-21 11:19:23 +0100

    move fonts to Bootstrap vars

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -23,19 +23,3 @@ export default {
 }
 </script>
 
-<style>
-html {
-  font-family: var(--font-family-sans-serif);
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-body {
-  font-family: var(--font-family-sans-serif);
-}
-</style>
