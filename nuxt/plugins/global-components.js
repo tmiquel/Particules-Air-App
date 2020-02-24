a11bf0c4f9312d16babed65ef4f859891808dba8
Thibaut Miquel <thibaut.miquel@gmail.com>
2020-02-24 11:03:18 +0100

    feat - add AppLandingBanner cmp

--- nuxt/plugins/global-components.js
+++ nuxt/plugins/global-components.js
@@ -3,9 +3,11 @@ import Vue from "vue";
 import AppButton from "@/components/UI/AppButton";
 import StakeholderLink from "@/components/UI/StakeholderLink";
 import SourceLink from "@/components/UI/SourceLink";
-import AppHeading from '@/components/UI/AppHeading'
+import AppHeading from '@/components/UI/AppHeading';
+import AppLandingBanner from '@/components/UI/AppLandingBanner';
 
 Vue.component("AppButton", AppButton);
 Vue.component("StakeholderLink", StakeholderLink);
 Vue.component("SourceLink", SourceLink);
-Vue.component('AppHeading', AppHeading)
+Vue.component('AppHeading', AppHeading);
+Vue.component('AppLandingBanner', AppLandingBanner);
