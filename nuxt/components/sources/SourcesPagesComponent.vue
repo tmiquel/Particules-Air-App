bd4004f15cb1b2c7c81a2d886a539898fb184367
Thibaut Miquel <thibaut.miquel@gmail.com>
2020-02-25 16:14:57 +0100

    wip - thibaut - issue: undefined route for to props of nuxt-link"

---
+++ nuxt/components/sources/SourcesPagesComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <app-landing-banner />
+    <app-heading>LES SOURCES</app-heading>
+
+    <b-card-group columns class="container">
+      <source-card :source="source" v-for="(source, id) in sources" :key="id" />
+    </b-card-group>
+    <back-to-post-link :current-route="this.currentRoute"/>
+  </div>
+</template>
+
+<script>
+import SourceCard from '@/components/sources/SourceCard'
+import sources from '~/data/sources.yml'
+import BackToPostLink from '@/components/posts/BackToPostLink'
+
+export default {
+  props: {
+    currentRoute: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      sources: sources
+    }
+  },
+  components: {
+    SourceCard,
+    BackToPostLink
+  }
+}
+</script>
