136f45dff9b3ab1b5e629fd9bb5e90c06c190198
Thibaut Miquel <thibaut.miquel@gmail.com>
2020-02-27 13:56:45 +0100

    feat - add basic graph page layout (without graphs yet)

---
+++ nuxt/pages/posts/_name/graphs.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <app-landing-banner></app-landing-banner>
+    <app-heading>LES GRAPHES</app-heading>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <back-to-post-link></back-to-post-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
