95cc69a10633e4067c9708b05f8e481cbdc7784d
Aminesanhaji <thpamine@gmail.com>
2020-02-28 18:38:28 +0100

    add- article enfants done here-with giphy-

--- nuxt/components/posts/Enfants.vue
+++ nuxt/components/posts/Enfants.vue
@@ -1,7 +1,40 @@
 <template>
   <div>
     <app-heading>ICI LE 3eme article</app-heading>
-    <p>Sur les 4,2 millions de morts prématurées dues à la pollution de l’air ambiant en 2016 dans le monde, 300 000 sont survenues chez des enfants de moins de 5 ans. La pollution de l’air est responsable de plus de la moitié de tous les décès par infections respiratoires basses chez les moins de 5 ans dans les pays en développement.</p>
+    <div style="width:100%;height:0;padding-bottom:56%;position:relative;">
+      <iframe
+        src="https://giphy.com/embed/xT39D1J4aZpV0stFnO"
+        width="100%"
+        height="100%"
+        style="position:absolute"
+        frameborder="0"
+        class="giphy-embed"
+        allowfullscreen
+      ></iframe>
+    </div>
+    <p>
+      <a href="https://giphy.com/gifs/petitbateauofficial-funny-lol-xT39D1J4aZpV0stFnO">via GIPHY</a>
+    </p>
+    <p
+      class="text-justify"
+    >Sur les 4,2 millions de morts prématurées dues à la pollution de l’air ambiant en 2016 dans le monde, 300 000 sont survenues chez des enfants de moins de 5 ans. La pollution de l’air est responsable de plus de la moitié de tous les décès par infections respiratoires basses chez les moins de 5 ans dans les pays en développement.</p>
+    <p
+      class="text-justify"
+    >Terrible constat mondial qui n’épargne guère les pays occidentaux et en particulier les régions globalement touchées par la pollution de l’air.</p>
+    <p
+      class="text-justify"
+    >Pour la zone autour de Marseille, Greenpeace avait publié une carte avec les données de Atmosud avec les écoles de Marseille.</p>
+    <p class="text-justify">
+      Nous vous remettons le lien de la carte en question :
+      <a
+        href="https://www.greenpeace.fr/pollution-ecole/marseille/"
+      >
+        <i>Par ici</i>
+      </a>
+    </p>
+    <p
+      class="text-justify"
+    >Pour rappel, A Marseille, “22% des écoles et crèches sont à moins de 50 mètres d’une concentration illégale de dioxyde d’azote, et 58% à moins de 200 mètres. C’est alarmant, d’autant plus que le respect des normes ne garantit pas aujourd’hui l’absence de risques pour la santé” , explique Sarah Fayolle, chargée de campagne Pollution de l’air et Transports à Greenpeace France.</p>
   </div>
 </template>
 
