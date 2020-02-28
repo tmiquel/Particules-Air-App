e50a00d8144015895f99d2b97917562d80e1b23b
Aminesanhaji <thpamine@gmail.com>
2020-02-28 17:21:05 +0100

    starting article enfants here

--- nuxt/components/posts/Enfants.vue
+++ nuxt/components/posts/Enfants.vue
@@ -1,3 +1,7 @@
 <template>
-  <app-heading>ICI LE 3eme article</app-heading>
+  <div>
+    <app-heading>ICI LE 3eme article</app-heading>
+    <p>Sur les 4,2 millions de morts prématurées dues à la pollution de l’air ambiant en 2016 dans le monde, 300 000 sont survenues chez des enfants de moins de 5 ans. La pollution de l’air est responsable de plus de la moitié de tous les décès par infections respiratoires basses chez les moins de 5 ans dans les pays en développement.</p>
+  </div>
 </template>
+
