6903d9abdd39924b28097cf6a46f5b5df984045b
Thibaut Miquel <thibaut.miquel@gmail.com>
2020-03-02 14:26:02 +0100

    wip thibaut - Emilie meeting

---
+++ nuxt/components/maps/MapsList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="mt-5">
+    <app-heading>Les graphes</app-heading>
+    <b-container>
+      <b-row :class="{'row-cols-1' : (this.$mq === 'mobile'), 'row-cols-2' : (this.$mq === 'desktop')}">
+        <b-col v-for="(graph, index) in graphs" :key="index">
+          <map-card :graph="graph"/>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+
+<script>
+import graphs from '~/data/graphs.yml'
+
+export default {
+  components: {
+    MapCard
+  },
+  data() {
+    return {
+      graphs: graphs
+    }
+  },
+}
+</script>
