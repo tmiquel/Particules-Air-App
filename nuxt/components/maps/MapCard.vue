946b9c5c9a0bb298b0ccf7a0401a0176842b21ef
Thibaut Miquel <thibaut.miquel@gmail.com>
2020-03-03 14:56:17 +0100

    feat - finish graph page and improve responsiveness of stakeholder slider

--- nuxt/components/maps/MapCard.vue
+++ nuxt/components/maps/MapCard.vue
@@ -1,17 +1,19 @@
 <template>
-  <b-card class="border-0 mt-2 mb-4" no-body overlay>
-    <map-preview :name="mapId" :has-margins="false" class="mb-0" />
+  <b-card class="border-0 mt-2 mb-4 pb-3" no-body overlay>
     <b-card-text :style="responsiveTitleFontSize" class="no-underline font-weight-bold gray text-dark">{{
       map.title
     }}</b-card-text>
-    <b-card-text :style="responsiveTextFontSize" class="no-underline gray text-dark">{{ map.description }}</b-card-text>
+    <map-preview :name="mapId" :has-margins="false" class="mb-0" />
     <!-- <b-card-body> -->
-    <stakeholder-link to="monique" class="card-link no-underline"
-      >Producteur des données : {{ stakeholders[map.data_producer_id].title }}
-    </stakeholder-link>
-    <b-link :href="sources[map.source_id].link" class="card-link  no-underline text-muted"
-      >Lien externe vers les données</b-link
-    >
+    <div class="d-flex justify-content-between py-1">
+      <stakeholder-link :to="map.data_producer_id" class="card-link no-underline" :style="responsiveTextFontSize"
+        >Source : {{ stakeholders[map.data_producer_id].title }}
+      </stakeholder-link>
+      <b-link :href="sources[map.source_id].link" class="card-link no-underline" :style="responsiveTextFontSize"
+        >Lien vers les données</b-link
+      >
+    </div>
+    <b-card-text :style="responsiveTextFontSize" class="no-underline gray text-dark">{{ map.description }}</b-card-text>
     <!-- </b-card-body> -->
   </b-card>
 </template>
