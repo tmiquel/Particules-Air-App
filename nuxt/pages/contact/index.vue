be60701e89a833291041075f07374a6bb7a4b0cc
Aminesanhaji <thpamine@gmail.com>
2020-03-05 18:37:09 +0100

    refact-change page Thanks here

--- nuxt/pages/contact/index.vue
+++ nuxt/pages/contact/index.vue
@@ -7,7 +7,7 @@
         method="post"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
-        action="/thanks"
+        action="/thankyou"
       >
         <input type="hidden" name="form-name" value="contact" />
         <b-form-row class="mb-4">
