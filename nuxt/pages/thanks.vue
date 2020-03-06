1fe893dd69a42e1a6de3f5f022e89cc4b1983b08
Aminesanhaji <thpamine@gmail.com>
2020-03-06 14:42:32 +0100

    page thanks and contact done here

--- nuxt/pages/thanks.vue
+++ nuxt/pages/thanks.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>
-    <br />
-    <br />
-    <app-heading class="mt-5">MERCI</app-heading>
-    <center>
-      <strong>Un membre de notre équipe vous contactera dans les plus brefs délais.</strong>
-    </center>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <app-heading class="mt-5">MERCI !</app-heading>
+        <center>
+          <strong>Un membre de notre équipe vous contactera dans les plus brefs délais.</strong>
+        </center>
+        <br />
+        <br />
+        <center>
+          <router-link to="/">
+            <button
+              class="btn rounded-pill font-weight-bold border-0 mt-2 mb-4 text-white mt-4"
+            >Retour à l'accueil</button>
+          </router-link>
+        </center>
+      </div>
+    </div>
   </div>
 </template>
+
+
+<style scoped>
+button {
+  background: linear-gradient(
+    89.86deg,
+    var(--left-navbar-gradient-color) -79.71%,
+    var(--right-navbar-gradient-color) 167.69%
+  );
+  padding-left: 2rem !important;
+  padding-right: 2rem !important;
+}
+</style>
+
+
+
+
 
