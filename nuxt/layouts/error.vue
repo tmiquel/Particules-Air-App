5acc910865f74b9881bdc9d561f71dc84574207d
Aminesanhaji <thpamine@gmail.com>
2020-03-12 17:48:41 +0100

    test2 add error.vue to Layout

---
+++ nuxt/layouts/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">Page non trouvée</h1>
+    <h1 v-else>Une erreur s'est produite</h1>
+    <nuxt-link to="/">Accueil</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: '404' // vous pouvez définir une mise en page personnalisée pour la page d'erreur
+}
+</script>
