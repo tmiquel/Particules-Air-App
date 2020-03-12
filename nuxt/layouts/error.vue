9b3d820264e21e7943d9879bbddca6544e6d090e
Aminesanhaji <thpamine@gmail.com>
2020-03-12 18:51:49 +0100

    custom error page DONE here

--- nuxt/layouts/error.vue
+++ nuxt/layouts/error.vue
@@ -6,21 +6,23 @@
     <br />
     <div class="errorpage row row-cols-2">
       <div class="col">
-        <app-heading v-if="error.statusCode === 404">Houston, we’ve had a problem here.</app-heading>
+        <app-sub-heading v-if="error.statusCode === 404">Ooups !</app-sub-heading>
         <app-heading v-else>Une erreur s'est produite</app-heading>
       </div>
     </div>
-    <div style="width:100%;height:0;padding-bottom:75%;position:relative;">
-      <img
-        src="https://giphy.com/embed/pH3KMJtWheyT6"
-        width="39%"
-        height="39%"
-        style="position:absolute"
-        frameborder="0"
-        class="giphy-embed"
-        allowfullscreen
-      />
+    <div
+      class="tenor-gif-embed"
+      data-postid="4780646"
+      data-share-method="host"
+      data-width="100%"
+      data-aspect-ratio="2.3380281690140845"
+    >
+      <a
+        href="https://tenor.com/view/apollo13-tom-hanks-houston-we-have-aproblem-problem-worried-gif-4780646"
+      >Houston We Have A Problem GIF</a> from
+      <a href="https://tenor.com/search/apollo13-gifs">Apollo13 GIFs</a>
     </div>
+    <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
     <button class="btn rounded-pill font-weight-bold border-0 mt-2 mb-4 text-white mt-4">
       <nuxt-link id="link" to="/">Retour à l'accueil</nuxt-link>
     </button>
