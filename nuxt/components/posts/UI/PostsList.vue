714ed014c8a4821c02efd2448e4346d07e5bf1d1
Thibaut Miquel <thibaut.miquel@gmail.com>
2020-03-23 16:28:56 +0100

    fix - postslist do not display current post and post banner works on Home Pages

--- nuxt/components/posts/UI/PostsList.vue
+++ nuxt/components/posts/UI/PostsList.vue
@@ -3,7 +3,7 @@
     <app-heading>Les articles</app-heading>
     <b-container>
       <b-row :no-gutters="true" class="row-cols-2 row-cols-md-3 row-cols-lg-4">
-        <b-col v-for="(titleObject, route, index) in postsTitles" :key="index">
+        <b-col v-for="(titleObject, route, index) in shownPosts" :key="index">
           <post-card :title="titleObject.title" :route="route" />
         </b-col>
       </b-row>
@@ -22,6 +22,20 @@ export default {
   data() {
     return {
       postsTitles
+    }
+  },
+  computed: {
+    shownPosts() {
+      let shownPosts = Object.assign({}, this.postsTitles)
+      delete shownPosts['home']
+      if (this.$route.params.name) {
+        delete shownPosts[this.$route.params.name]
+      } else {
+        // we are in the Home page
+        let homePostKey = this.postsTitles['home']['redirect-to']
+        delete shownPosts[homePostKey]
+      }
+      return shownPosts
     }
   }
 }
