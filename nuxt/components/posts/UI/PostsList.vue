3dc7875d1ad8e19883261baa68d20eabb9b8524d
Thibaut Miquel <thibaut.miquel@gmail.com>
2020-03-23 11:10:49 +0100

    refactor - move Posts titles as they appea r in the posts list from Vuex to yml db

--- nuxt/components/posts/UI/PostsList.vue
+++ nuxt/components/posts/UI/PostsList.vue
@@ -3,8 +3,8 @@
     <app-heading>Les articles</app-heading>
     <b-container>
       <b-row :no-gutters="true" class="row-cols-2 row-cols-md-3 row-cols-lg-4">
-        <b-col v-for="(title, route, index) in postsTitle" :key="index">
-          <post-card :title="title" :route="route" />
+        <b-col v-for="(titleObject, route, index) in postsTitles" :key="index">
+          <post-card :title="titleObject.title" :route="route" />
         </b-col>
       </b-row>
     </b-container>
@@ -13,14 +13,16 @@
 
 
 <script>
-import { mapState } from 'vuex'
+import postsTitles from '~/data/posts-titles.yml'
 import PostCard from '@/components/posts/UI/PostCard'
 export default {
-  computed: mapState({
-    postsTitle: state => state.postsTitle
-  }),
   components: {
     PostCard
+  },
+  data() {
+    return {
+      postsTitles
+    }
   }
 }
 </script>
