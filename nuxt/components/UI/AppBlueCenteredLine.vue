8f4e4ab3c71107d1ea6791bde596e5d5cab1edb5
Thibaut Miquel <thibaut.miquel@gmail.com>
2020-03-25 20:32:01 +0100

    wip desktop About page Thibaut - missing less margins-x for pics

---
+++ nuxt/components/UI/AppBlueCenteredLine.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="line mx-auto ">&nbsp;</div>
+</template>
+
+<style scoped>
+/* https://stackoverflow.com/questions/11239747/css-border-bottom-in-the-middle */
+ .line {
+    width: 112px;
+    height: 8px;
+    /* margin: 0px auto; */
+    border-bottom: 8px solid var(--primary);
+    margin-top: 76px;
+    margin-bottom: 28px;
+ }
+</style>
--- nuxt/components/UI/AppHeading.vue
+++ nuxt/components/UI/AppHeading.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="d-flex justify-content-center">
-    <h5 id="app-title" class="text-uppercase mb-4 mb-md-5" :style="responsiveHeadingFontSize">
+    <h5 id="app-title-mobile" class="text-uppercase mb-4 mb-md-5" :style="responsiveHeadingFontSize" v-if="$mq === 'mobile'">
       <slot></slot>
     </h5>
+
+    <h1 id="app-title-desktop" class="display-2 text-uppercase mb-4 mb-md-5" v-if="$mq === 'desktop'">
+      <slot></slot>
+      <app-blue-centered-line />
+    </h1>
   </div>
 </template>
 
@@ -19,8 +24,8 @@ export default {
 }
 </script>
 
-<style scoped>
-#app-title {
+<style scoped lang='scss'>
+#app-title-mobile {
   background-image: linear-gradient(
     to right,
     var(--left-gradient-color),
@@ -35,6 +40,11 @@ export default {
   font-weight: bold;
 
   color: var(--font-headers-color);
+}
+
+#app-title-desktop {
+  font-weight: bold;
+  color: var(--dark-grey);
 }
 </style>
 
