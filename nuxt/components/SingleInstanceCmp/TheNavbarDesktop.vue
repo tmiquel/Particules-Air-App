9ee1eaf6224e131d3c572c6a21ad293632a7f29b
Aminesanhaji <thpamine@gmail.com>
2020-03-26 13:01:18 +0100

    desktop navbar here, needs to center title

---
+++ nuxt/components/SingleInstanceCmp/TheNavbarDesktop.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" class="navbar">
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav active class="text">
+        <b-nav-item href="/about">A propos</b-nav-item>
+        <b-nav-item href="/contact">contact</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+    <b-navbar-brand class="title mx-auto" active href="/">Particules</b-navbar-brand>
+  </b-navbar>
+</template>
+
+
+
+
+<style scoped>
+.navbar {
+  background: #ffffff; /*#ffffff later */
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+}
+.title {
+  font-family: cursive;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 35px;
+  line-height: 42px;
+
+  /* identical to box height */
+
+  color: #016fbf;
+}
+.text {
+  font-family: cursive;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 17px;
+  line-height: 20px;
+}
+.navbar-dark .navbar-nav .nav-link {
+  color: #454545 !important;
+}
+</style>
