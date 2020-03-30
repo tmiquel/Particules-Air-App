a135fab6fe9953aea6543259dd8c8f3f6cec37db
Thibaut Miquel <thibaut.miquel@gmail.com>
2020-03-30 11:37:44 +0200

    refactor - remove carousel code from about/ index (transferred in ParticulesTeamCarouselModal.vue)

--- nuxt/pages/about/index.vue
+++ nuxt/pages/about/index.vue
@@ -72,89 +72,6 @@
       </b-row>
     </b-container>
     <b-container>
-      <a-carousel arrows>
-        <div slot="prevArrow" slot-scope="props" class="custom-slick-arrow" style="left: 30px;zIndex: 1">
-          <a-icon type="left" />
-        </div>
-        <div slot="nextArrow" slot-scope="props" class="custom-slick-arrow" style="right: 55px">
-          <a-icon type="right" />
-        </div>
-        <b-container v-for="(particulesTeamMember, particulesTeamMemberId, index) in techTeam" :key="index">
-          <b-row class="row-cols-2 py-5" id="carousel-content">
-            <b-col cols="5" class="pl-5">
-              <cld-image
-                class="d-flex justify-content-center"
-                responsive="width"
-                :publicId="imgPublicIds[particulesTeamMemberId]"
-                :alt="particulesTeamMember.name"
-              >
-                <!-- https://github.com/cloudinary/cloudinary-vue/blob/master/src/components/CldImage/CldImage.md -->
-                <!-- https://cloudinary.com/documentation/image_transformation_reference -->
-
-                <cld-transformation width="646" height="772" crop="fill" effect="improve" />
-                <cld-transformation
-                  width="auto"
-                  gravity="face:center"
-                  dpr="auto"
-                  fetchFormat="auto"
-                  quality="auto:best"
-                />
-              </cld-image>
-            </b-col>
-            <b-col cols="7" class="pl-md-5 d-flex flex-column justify-content-start align-items-left">
-              <h5 :style="{ fontSize: '18px' }" class="text-primary text-left text-uppercase">
-                <b>{{ particulesTeamMember.squad }}</b>
-              </h5>
-              <span class="text-left py-3" :style="{ fontSize: '25px' }">
-                <b v-html="particulesTeamMember.name"></b>
-              </span>
-              <div class="blueline mr-auto">&nbsp;</div>
-              <p class="text-left" :style="{ fontSize: '22px' }">{{ particulesTeamMember.jobtitle }}</p>
-              <p :style="{ fontSize: '22px' }" class="text-left pt-3" v-html="particulesTeamMember.description"></p>
-
-              <span :style="{ width: '55%' }" class="d-inline-flex justify-content-between">
-                <div :style="{cursor: 'pointer'}" v-b-hover="handleHoverEmail" v-if="particulesTeamMember.email">
-                  <a target="_blank" :href="'mailto:'+particulesTeamMember.email">
-                  <a-icon type="mail" :theme="isEmailHovered ? 'filled' : 'outlined'" :style="{ fontSize: '30px' }" />
-                  </a>
-                </div>
-                <div :style="{cursor: 'pointer'}" v-b-hover="handleHoverLinkedin" v-if="particulesTeamMember.linkedin">
-                  <a target="_blank" :href="particulesTeamMember.linkedin">
-                  <a-icon
-                    type="linkedin"
-                    :theme="isLinkedinHovered ? 'outlined' : 'filled'"
-                    :style="{ fontSize: '30px', color: '#0e76a8' }"
-                    fill="#0e76a8"
-                  />
-                  </a>
-                </div>
-                <div :style="{cursor: 'pointer'}" v-b-hover="handleHoverTwitter" v-if="particulesTeamMember.twitter">
-                  <a target="_blank" :href="particulesTeamMember.twitter">
-                  <a-icon
-                    type="twitter"
-                    :style="{ fontSize: '30px', color: '#00acee' }"
-                    fill="#00acee"
-                  />
-                  </a>
-                </div>
-                <div :style="{cursor: 'pointer'}" v-b-hover="handleHoverFacebook" v-if="particulesTeamMember.facebook">
-                  <a target="_blank" :href="particulesTeamMember.facebook">
-                  <a-icon
-                    type="facebook"
-                    :theme="isFacebookHovered ? 'outlined' : 'filled'"
-                    :style="{ fontSize: '30px', color: '#3b5998' }"
-                    fill="#3b5998"
-                  />
-                  </a>
-                </div>
-              </span>
-            </b-col>
-          </b-row>
-        </b-container>
-      </a-carousel>
-    </b-container>
-
-    <b-container>
       <app-sub-heading :isCenteredOnDesktop="true" class="mt-5 mb-md-5 text-center"
         >côté tech et data visualisation</app-sub-heading
       >
@@ -227,7 +144,6 @@
 </template>
 
 <script>
-import { Carousel, Icon } from 'ant-design-vue'
 import ParticulesTeamMemberCard from '@/components/about/ParticulesTeamMemberCard'
 import particulesTeamMembers from '~/data/particules-team.yml'
 import ParticulesTeamMemberSlider from '~/components/about/ParticulesTeamMemberSlider.vue'
@@ -243,11 +159,7 @@ export default {
       nous avons toutes et tous été sensibilisés à la pollution de l’air \
       que ce soit par les média ou des articles\
        scientifiques mais finalement très peu informés des dangers sur notre santé \
-       que celle-ci pouvait constituer.',
-      isTwitterHovered: false,
-      isFacebookHovered: false,
-      isLinkedinHovered: false,
-      isEmailHovered: false
+       que celle-ci pouvait constituer.'
     }
   },
   components: {
@@ -257,8 +169,6 @@ export default {
   }
 }
 </script>
-
-
 
 <style lang="scss" scoped>
 p {
@@ -281,51 +191,5 @@ a {
   font-size: 25px;
   color: var(--dark-grey);
 }
-
-#carousel-content {
-  padding-top: 118px !important;
-  padding-left: 165px !important;
-  padding-right: 165px !important;
-  padding-bottom: 118px !important;
-}
-
-.blueline {
-  width: 65px;
-  height: 8px;
-  /* margin: 0px auto; */
-  border-bottom: 5px solid var(--primary);
-  margin-top: 7px;
-  margin-bottom: 18px;
-}
-</style>
-
-<style scoped>
-/* Second style tag because deep selector don't work with scss */
-.ant-carousel >>> .slick-slide {
-  text-align: center;
-  /* height: 160px; */
-  /* line-height: 160px; */
-  overflow: hidden;
-}
-
-.ant-carousel >>> .custom-slick-arrow {
-  width: 25px;
-  height: 50px;
-  font-size: 50px;
-  color: var(--dark-grey);
-  /* background-color: rgba(31, 45, 61, 0.11); */
-  opacity: 0.7;
-}
-.ant-carousel >>> .custom-slick-arrow:before {
-  display: none;
-}
-.ant-carousel >>> .custom-slick-arrow:hover {
-  opacity: 1;
-}
-
-.ant-carousel >>> .slick-slide h3 {
-  color: #fff;
-}
-
 
 </style>
